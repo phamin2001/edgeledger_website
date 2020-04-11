30a85fe60b58231a6e6f6f20a2d907a896c071c1
phamin2001 <amin.pahlavani@gmail.com>
2020-04-11 13:23:08 -0600

    Smooth Scrolling with JS and JQ

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function initMap() {
   // Your location
   const loc = {
     lat: 39.739235,
-    lng: -104.99025
+    lng: -104.99025,
   };
   // Current map on location
   const map = new google.maps.Map(document.querySelector('.map'), {
@@ -16,3 +16,28 @@ function initMap() {
     map: map,
   });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
